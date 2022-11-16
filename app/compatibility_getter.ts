af8e1be1ef17b21876f370a408dbae4368827c94
kacper701 <93781322+kacper701@users.noreply.github.com>
2022-11-16 21:07:15 +0100

    cos

--- app/compatibility_getter.ts
+++ app/compatibility_getter.ts
@@ -9,6 +9,9 @@ const Get = (a:string,b:string) => {
     if (a == "INTP" && b == "INTP") {
         return Goodness.Best;
     }
+    if (a == "INTP" && b == "INTJ") {
+        return Goodness.Bad;
+    }
     // Add rest of the cases
     return Goodness.Bad;
 }
